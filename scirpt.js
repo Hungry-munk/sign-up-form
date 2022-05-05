f2da4d56aef4c212ac66889e33a1d6c389be847c
Hungry_munk <abbaszadeharsam@gmail.com>
2022-05-05 18:13:30 +1000

    regex completed and can now add classes and remove them based on valid or invalid using regex

--- scirpt.js
+++ scirpt.js
@@ -6,13 +6,23 @@ const Password = document.getElementById('password')
 const ConfirmPassword = document.getElementById('confirm-password')
 
 const EMAIL_REGEX = /[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.com/g
-let typingtimer;
+const NAME_REGEX = /^[A-Z][a-z]*(([,.] |[ '-])[A-Za-z][a-z]*)*(\.?)$/g
+const AUS_PHONE_REGEX = /(04[0-9]{8}){1}/g
+const PASSWORD_REGEX = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%-_^&*\.])[a-zA-Z0-9!@#$%-_^&*\.]{8,25}/g
+const USERNAME_REGEX = /(?=.*[a-z])[a-zA-Z0-9$#^&%*\.]{3,20}/g
 
+let tpyingDelayReaction = 1500
+let typingtimer;
 Email.addEventListener('input', ()=>{
     clearTimeout(typingtimer)
     if (Email.value){
         typingtimer = setTimeout(()=>{
-            console.log(Email.value,"\n this fucking working\n",)
-        }, 2000)
-    }
-})
+            Email.classList.add('invalid')
+            Email.classList.remove('valid')
+            if(Email.value.match(EMAIL_REGEX)){
+                Email.classList.remove('invalid')
+                Email.classList.add('valid')
+            }
+        }, tpyingDelayReaction);
+    };
+});
