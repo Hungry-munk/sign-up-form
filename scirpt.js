c0622ffbf1e34f57a299af8cec77621111c5ea46
Hungry_munk <abbaszadeharsam@gmail.com>
2022-05-05 20:35:34 +1000

    made thinghs valid when you enter a correct input using an object

--- scirpt.js
+++ scirpt.js
@@ -6,23 +6,53 @@ const Password = document.getElementById('password')
 const ConfirmPassword = document.getElementById('confirm-password')
 
 const EMAIL_REGEX = /[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.com/g
-const NAME_REGEX = /^[A-Z][a-z]*(([,.] |[ '-])[A-Za-z][a-z]*)*(\.?)$/g
+const NAME_REGEX = /[A-Z][a-z]+/
 const AUS_PHONE_REGEX = /(04[0-9]{8}){1}/g
 const PASSWORD_REGEX = /(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%-_^&*\.])[a-zA-Z0-9!@#$%-_^&*\.]{8,25}/g
-const USERNAME_REGEX = /(?=.*[a-z])[a-zA-Z0-9$#^&%*\.]{3,20}/g
+const USERNAME_REGEX = /(?=.*[a-z])[a-zA-Z0-9$*\.]{3,20}/g
 
-let tpyingDelayReaction = 1500
+const inputFields = {
+    username : [Username,USERNAME_REGEX],
+    name : [Name,NAME_REGEX],
+    email : [Email,EMAIL_REGEX],
+    phone : [Phone, AUS_PHONE_REGEX],
+    password : [Password,PASSWORD_REGEX],
+    confirmPassword : [ConfirmPassword, PASSWORD_REGEX] 
+}
+
+let tpyingDelayReaction = 1000
 let typingtimer;
-Email.addEventListener('input', ()=>{
-    clearTimeout(typingtimer)
-    if (Email.value){
-        typingtimer = setTimeout(()=>{
-            Email.classList.add('invalid')
-            Email.classList.remove('valid')
-            if(Email.value.match(EMAIL_REGEX)){
-                Email.classList.remove('invalid')
-                Email.classList.add('valid')
-            }
+let typingtimer2
+
+Email.addEventListener('input', ()=>checkValid('email' , typingtimer));
+Phone.addEventListener('input', ()=>checkValid('phone', typingtimer))
+Username.addEventListener('input', ()=>checkValid('username', typingtimer))
+Name.addEventListener('input', ()=>checkValid('name', typingtimer))
+Password.addEventListener('input', ()=>checkValid('password', typingtimer))
+
+
+function checkValid (field, timer) {
+    clearTimeout(timer)
+    console.log('typed')
+    if(!inputFields[field][0].value){
+        inputFields[field][0].classList.remove('invalid')
+        inputFields[field][0].classList.remove('valid')
+        console.log('emptied')
+    }
+
+    else if (inputFields[field][0].value){
+        timer = setTimeout(()=>{
+            if (inputFields[field][0].value.match(inputFields[field][1])){
+                inputFields[field][0].classList.add('valid')
+                inputFields[field][0].classList.remove('invalid')
+                console.log('matched')
+            } else {
+                inputFields[field][0].classList.add('invalid')
+                inputFields[field][0].classList.remove('valid')
+                console.log('not matched')
+            };
+
         }, tpyingDelayReaction);
     };
-});
+
+};
