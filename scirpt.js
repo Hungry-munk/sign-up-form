22b1fc72c8e1f9b2d1df7a94949f17499a0e6c4d
Hungry_munk <abbaszadeharsam@gmail.com>
2022-05-06 21:47:17 +1000

    finally finsished everything, added the button diabling and enabling + some css to button

--- scirpt.js
+++ scirpt.js
@@ -4,6 +4,10 @@ const Email = document.getElementById('email')
 const Phone = document.getElementById('phone')
 const Password = document.getElementById('password')
 const ConfirmPassword = document.getElementById('confirm-password')
+const Form = document.querySelector('form')
+const SubmitBtn = document.getElementById('submit-btn')
+
+const inputBoxes = [Username, Name, Email, Phone, Password, ConfirmPassword]
 
 const EMAIL_REGEX = new RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.com$/)
 const AUS_PHONE_REGEX = new RegExp (/^(04[0-9]{8}){1}$/)
@@ -22,13 +26,27 @@ const inputFields = {
 
 let tpyingDelayReaction = 1000
 let typingtimer;
-let typingtimer2
+let timer2
 
 Email.addEventListener('input', ()=>checkValid('email' , typingtimer));
 Phone.addEventListener('input', ()=>checkValid('phone', typingtimer))
 Username.addEventListener('input', ()=>checkValid('username', typingtimer))
 Name.addEventListener('input', ()=>checkValid('name', typingtimer))
-Password.addEventListener('input', ()=>checkValid('password', typingtimer))
+Password.addEventListener('input', ()=>{checkValid('password', typingtimer);checkPasswords()});
+//checking for confirm password input incase the enter password their first
+ConfirmPassword.addEventListener('input', ()=> checkPasswords());
+
+let ValidForm
+Form.addEventListener('change', ()=>{
+    if (inputBoxes.every(field => field.checkValidity())) {
+        SubmitBtn.removeAttribute('disabled')
+        console.log('btn enabled')
+    } else {
+        SubmitBtn.setAttribute('disabled', 'disabled')
+        console.log('btn disabled')
+    };
+});
+
 
 
 function checkValid (field, timer) {
@@ -37,7 +55,7 @@ function checkValid (field, timer) {
     if(!inputFields[field][0].value){
         inputFields[field][0].classList.remove('invalid')
         inputFields[field][0].classList.remove('valid')
-        console.log('emptied')
+        inputFields[field][0].setCustomValidity('')
     }
 
     else if (inputFields[field][0].value){
@@ -45,16 +63,49 @@ function checkValid (field, timer) {
             if (inputFields[field][1].test(inputFields[field][0].value)){
                 inputFields[field][0].classList.add('valid')
                 inputFields[field][0].classList.remove('invalid')
-                console.log('matched')
+                inputFields[field][0].setCustomValidity('')
+
             } else {
                 inputFields[field][0].classList.add('invalid')
                 inputFields[field][0].classList.remove('valid')
-                console.log('not matched')
-                Email.setCustomValidity(' ')
-                Email.reportValidity()
+                inputFields[field][0].setCustomValidity(' ')//making form invalid
+
             };
 
         }, tpyingDelayReaction);
     };
 
+};
+
+function checkPasswords (){
+    clearTimeout(timer2)
+    
+    if (ConfirmPassword.value !== ''){
+        timer2 = setTimeout(()=>{
+
+            if (Password.value === ConfirmPassword.value && PASSWORD_REGEX.test(ConfirmPassword.value)) {
+                ConfirmPassword.classList.add('valid')
+                ConfirmPassword.classList.remove('invalid')
+                ConfirmPassword.setCustomValidity('')
+                console.log('vlaid')
+
+            } else {
+                ConfirmPassword.classList.add('invalid')
+                ConfirmPassword.classList.remove('valid')
+                console.log('not vlaid')
+                if (Password.value){
+                    ConfirmPassword.setCustomValidity('Not the same password')
+                    ConfirmPassword.reportValidity()
+                } else {
+                    ConfirmPassword.setCustomValidity('you need to enter a password')
+                    ConfirmPassword.reportValidity()
+                }
+            }
+        }, tpyingDelayReaction);
+    }
+
+    if (ConfirmPassword.value == ''){
+        ConfirmPassword.classList.remove('invalid')
+        ConfirmPassword.classList.remove('valid')
+    }
 };
